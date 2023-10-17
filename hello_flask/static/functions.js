3c0b9ff8f859eef7bbff7ad4e3225010192bf488
Mike Zeng <97768962+Mai1aKi@users.noreply.github.com>
2023-10-17 00:35:26 -0400

    Add files via upload
    part 2 display update

--- hello_flask/static/functions.js
+++ hello_flask/static/functions.js
@@ -1,3 +1,30 @@
 function welcome() {
     document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀"
+
+    updatePosts();
+    setInterval(updatePosts, 2000);
 }
+
+function updatePosts() {  // update post every 2 sec
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+            const postsSection = document.querySelector('.posts-section');
+            let posts = '<h3>Posts RIGHT HERE!</h3>';
+            data.forEach(post => {
+                posts += `
+                <div class="post">
+                    <h3>${post.title}</h3>
+                    <p>${post.description}</p>
+                    <small>Posted by: ${post.username}</small>
+                </div>`;
+            });
+            postsSection.innerHTML = posts;
+        }
+    };
+    request.open("GET", "/get_posts");
+    request.send();
+}
+
+
