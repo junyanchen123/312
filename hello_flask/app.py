a5278ee2594d1ba12e23c9c8be5d02d4200440c8
cpapa116 <cjgiampa@buffalo.edu>
2023-10-18 12:49:19 -0400

    rewrote frontend, added comments for readability -christian

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,86 +1,86 @@
 import html
-import json
-
-from flask import Flask, make_response, request, session, render_template, redirect
-from pymongo import MongoClient
+import hashlib
 import bcrypt
-import uuid
+import json
+from flask import Flask, make_response, request, redirect
+from pymongo import MongoClient
+from uuid import uuid4
 
 mongo_client = MongoClient("mongo")
-db = mongo_client["cse312"]  # database
+db = mongo_client["cse312"]                                 #database                         
 
-security_collection = db["security"]  # collection in the database for usernames/passwords?
-token_collection = db["token"]  # collection in the database for ???
-post_collection = db["post"]  #
+security_collection = db["security"]                        #collection in the database for usernames/salts/password hashes/auth hashes
+post_collection = db["post"]                                #collection in the database for posts
 
-app = Flask(__name__)  # initialise the applicaton
-app.secret_key = "super secret key"
+app = Flask(__name__)                                       #initialise the applicaton
 
+#security_collection.delete_many({})                        #REMOVE THIS LINE
 
 @app.route("/")
 def home():
-    htmlCodeStream = open("templates/index.html", "rb")
+    htmlCodeStream = open("templates/index.html", "rb").read()                  #reads index.html as bytes
+    r = make_response(htmlCodeStream.decode())                                  #makes response to serve index.html
+    r.headers.set("X-Content-Type-Options", "nosniff")                          #sets nosniff header
+    return r
 
-    bodystring: bytes = htmlCodeStream.read()
+@app.route("/login.html")
+def logger():
+    loginPage = open("templates/login.html","rb").read()
+    response = make_response(loginPage)
+    response.headers.set("X-Content-Type-Options","nosniff")
+    response.headers.set("Content-Type","text/html")
+    return response
 
-    htmlCodeStream.close()
-
-    r = make_response(bodystring.decode())
-
-    r.headers.set("X-Content-Type-Options", "nosniff")
-
-    all_posts = list(post_collection.find({}))  # get post from db send to the home page
-
-    return render_template('index.html', username=session.get('username'), posts=all_posts)
-
-
-@app.route("/static/style.css")
-def home2():
-    cssCodeStream = open("static/style.css", "rb")
-
-    bodystring: bytes = cssCodeStream.read()
-
-    cssCodeStream.close()
-
-    r = make_response(bodystring.decode())
-
+@app.route("/index.css")
+def indexCsser():
+    cssCodeStream = open("templates/index.css", "rb").read()
+    r = make_response(cssCodeStream.decode())
     r.headers.set("X-Content-Type-Options", "nosniff")
     r.headers.set("Content-Type", "text/css")
-
     return r
 
+@app.route("/posts.html")
+def posterhtml():
+    posterer = open("templates/posts.html","rb").read()
+    response = make_response(posterer)
+    response.headers.set("Content-Type","text/html")
+    response.headers.set("X-Content-Type-Options","nosniff")
+    return response
 
-@app.route("/static/functions.js")
-def home3():
-    jsCodeStream = open("static/functions.js", "rb")
+@app.route("/posts.css")
+def posterthingy():
+    posterer = open("templates/posts.css","rb").read()
+    response = make_response(posterer)
+    response.headers.set("Content-Type","text/css")
+    response.headers.set("X-Content-Type-Options","nosniff")
+    return response
 
-    bodystring: bytes = jsCodeStream.read()
+@app.route("/finduser")
+def userLocator():
+    auth = request.cookies.get('auth')
+    hashAuth = hashSlingingSlasher(auth)
+    record = security_collection.find_one({"hashed authentication token":hashAuth})
+    username = record["username"]
+    response = make_response(username)
+    response.headers.set("X-Content-Type-Options","nosniff")
+    response.headers.set("Content-Type","text/plain")
+    return response
 
-    jsCodeStream.close()
-
-    r = make_response(bodystring.decode())
-
-    r.headers.set("X-Content-Type-Options", "nosniff")
-    r.headers.set("Content-Type", "text/javascript")
-
-    return r
-
+@app.route("/functions.js")
+def jsFunctions():
+    jsCodeStream = open("static/functions.js", "rb").read()
+    response = make_response(jsCodeStream)
+    response.headers.set("X-Content-Type-Options", "nosniff")
+    response.headers.set("Content-Type", "text/javascript")
+    return response
 
 @app.route("/static/about-us.jpg")
 def home4():
-    imageCodeStream = open("static/about-us.jpg", "rb")
-
-    bodystring: bytes = imageCodeStream.read()
-
-    imageCodeStream.close()
-
-    r = make_response(bodystring)
-
+    imageCodeStream = open("static/about-us.jpg", "rb").read()
+    r = make_response(imageCodeStream)
     r.headers.set("X-Content-Type-Options", "nosniff")
     r.headers.set("Content-Type", "image/jpg")
-
     return r
-
 
 @app.route("/visit-counter")
 def cookie():
@@ -101,28 +101,66 @@ def cookie():
 
     return response
 
+@app.route("/register", methods=['POST'])
+def register():
+    username = html.escape(str(request.form.get('reg_username')))                           #working, gets username from request
+    bPass = str(request.form.get('reg_password')).encode()                                  #password from request in bytes
+    salt = bcrypt.gensalt()                                                                 #salt used to hash password (we need this later)
+    hashPass = bcrypt.hashpw(bPass,salt)                                                    #salted and hashed password
 
+    registeredUsers = list(security_collection.find({'username':username}))                 #looks for the username in the database, needs to be converted to list to use
+    if len(registeredUsers) != 0:                                                           #this list is always len 1, the only element is one dictionary containing each user record
+        return redirect("/",301)                                                            #username is not available
+    else:                                                                                   
+        security_collection.insert_one({"username":username,"salt":salt,"hpw":hashPass})    #username is unique so it is inserted into the database
+        return redirect("/login.html",301)                                                  #username is available
+
+@app.route("/login", methods=['POST'])
+def login():
+    username = html.escape(str(request.form.get('log_username')))                           #gets username from the username textbox
+    password = str(request.form.get('log_password')).encode()                               #gets password from the password textbox
+    userRecord = list(security_collection.find({"username":username}))                      #looks up the username in the database and gets the unique user data
+    #^converts the user record into a list with ONE dictionary containing all of the user information
+    #referencing the user info will always need to be userRecord[0], this list will never be greater than size 1
+
+    if len(userRecord) != 0:
+        userInfo = userRecord[0]                                        #used to simplify notation, user information is always in index 0    
+        salt = userInfo["salt"]                                         #gets salt from the database record
+        realHash = userInfo["hpw"]                                      #gets the sha256 hash of the user's password from the database record
+        passHash = bcrypt.hashpw(password,salt)                         #gets the sha256 hash of the ENTERED password from the textbox
+        if passHash == realHash:                                        #user is logged in if both hashes match
+            token = str(uuid4())                                        #generate a random token using uuid4
+            tokenHash = hashSlingingSlasher(token)                      #hash this token for the database
+            security_collection.update_one({"username":username,"salt":salt,"hash":realHash},{"$set":{"hashed authentication token":tokenHash}},True) #updates database to include authenticated token hash in the record
+            response = make_response(redirect('/posts.html',301))       #generates response that will redirect to the posts page
+            response.set_cookie("auth",token,3600,httponly=True)        #sets authentication token as a cookie
+            return response
+        else:
+            return redirect("/login.html",301)                          #incorrect password
+    else:
+        return redirect("/login.html",301)                              #username not found
+    
 @app.route("/get_posts", methods=['GET'])
-def get_posts():
+def get_posts():                                #UNTESTED (pulled from most recent push)
     posts = list(post_collection.find({}))
     for post in posts:
         post["_id"] = str(post["_id"])
     return json.dumps(posts)
 
 
-@app.route("/add_post", methods=['POST'])
+@app.route("/add_post", methods=['POST'])       #UNTESTED (pulled from most recent push)
 def addPost():
-    token_str = request.cookies.get('token')
-    # token store in cookie is String, change it to int(in db is int)
+    token_str = request.cookies.get('auth')     #token is a now a string in the database
     try:
-        token = int(token_str)
+        token = str(token_str)                  #should already be str, if None it will fail
     except TypeError:  # if None no user log in
-        response = make_response("No user login", 400)
+        response = make_response("No user login", 401)
         response.headers.set("X-Content-Type-Options", "nosniff")
         response.headers.set("Content-Type", "text/plain")
         return response
 
-    userData = token_collection.find_one({"token": token})
+    userData = security_collection.find_one({"hashed authentication token": token})
+    print(userData)
     if not userData:
         response = make_response("Invalid token", 401)  # un-auth
         response.headers.set("X-Content-Type-Options", "nosniff")
@@ -130,15 +168,16 @@ def addPost():
         return response
 
     username = userData.get('username')
-
-    title = request.form.get('post_title')
-    description = request.form.get('post_description')
-
+    print(username)
+    title = request.form.get('title')
+    print(title)
+    message = request.form.get('message')
+    print(message)
     post_collection.insert_one({
         "title": html.escape(title),
-        "description": html.escape(description),
+        "message": html.escape(message),
         "username": html.escape(username),
-        "mesID": str(uuid.uuid4())
+        "mesID": str(uuid4())
     })
 
     response = make_response("Post Success")
@@ -146,89 +185,11 @@ def addPost():
     response.headers.set("Content-Type", "text/plain")
     return response
 
-
-@app.route("/register", methods=['POST'])
-def register():
-    username = html.escape(str(request.form.get('reg_username')))
-    bPass = str(request.form.get('reg_password')).encode()
-    salt = bcrypt.gensalt()
-    hashPass = bcrypt.hashpw(bPass, salt)
-
-    uniqueUser = True
-
-    for x in security_collection.find():
-        databaseUser = x["username"]
-        if databaseUser == username:
-            uniqueUser = False
-
-    if (not uniqueUser):
-        bodystring = "Invalid register. Must be unique"
-
-        response = make_response(bodystring)
-
-        response.headers.set("X-Content-Type-Options", "nosniff")
-        response.headers.set("Content-Type", "text/plain")
-        return response
-    if (uniqueUser):
-        security_collection.insert_one({"username": username, "salt": salt, "hpw": hashPass})
-        bodystring = username + " has been registered"
-
-        response = make_response(bodystring)
-
-        response.headers.set("X-Content-Type-Options", "nosniff")
-        response.headers.set("Content-Type", "text/plain")
-
-        return response
-
-
-@app.route("/login", methods=['POST'])
-def login():
-    # i did str on the request form data since I dont know what type original data is. Probably JSON
-    username = html.escape(str(request.form.get('log_username')))
-
-    userornah = False  # false if not user. True if user.
-
-    password = str(request.form.get('log_password'))  # un-hashed/salted password
-
-    passwordInBytes = password.encode('utf-8')
-
-    for x in security_collection.find():
-        databaseUsername = x["username"]
-
-        databasePassword = x["hpw"]
-
-        databaseSalt = x["salt"]
-        if (username == databaseUsername and bcrypt.hashpw(passwordInBytes, databaseSalt) == databasePassword):
-            userornah = True
-
-    if userornah:
-        # set the token and put it in the token_collection part of the DB
-
-        randomTokenString = bcrypt.gensalt()  # just a random string
-        randomHashedTokenString = hash(randomTokenString)
-        token_collection.insert_one({"username": (html.escape(username)), "token": randomHashedTokenString})
-
-        bodystring = username + " has been logged in"
-
-        response = make_response(bodystring)
-
-        response.headers.set("X-Content-Type-Options", "nosniff")
-        response.headers.set("Content-Type", "text/plain")
-        response.set_cookie("token", str(randomHashedTokenString), max_age=3600, httponly=True)
-        return response
-
-
-    else:
-        # Invalid login. Just let them know, no token here.
-        bodystring = "Invalid login. Try again"
-
-        response = make_response(bodystring)
-
-        response.headers.set("X-Content-Type-Options", "nosniff")
-        response.headers.set("Content-Type", "text/plain")
-
-        return response
-
+def hashSlingingSlasher(token):                                                 #wrapper for hashlib256
+    object256 = hashlib.sha256()
+    object256.update(token.encode())
+    tokenHash = object256.digest()
+    return(tokenHash)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)  # any time files change automatically refresh
--- hello_flask/static/functions.js
+++ hello_flask/static/functions.js
@@ -1,11 +1,21 @@
-function welcome() {
-    document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀"
+//rewrote frontend
 
-    updatePosts();
-    setInterval(updatePosts, 2000);
+function user(){                                                    //gets the username for the frontend display
+    const request = new XMLHttpRequest();                           //generates an XMLHttpRequest for later use
+    request.onreadystatechange = function() {                       
+        if(this.readyState === 4 && this.status === 200){           //when a 200 response is received resume here
+            var username = this.response;                           //gets username from python response
+            var uPostField = document.getElementById("username");   //gets the post field that normally would say "username"
+            var uHead = document.getElementById("uHeader");         //gets the post field that normally would say "username's Feed"
+            uHead.innerHTML = (username + "'s Feed");               //updates username frontend
+            uPostField.innerHTML = username;                        //updates username frontend
+        }
+    }
+    request.open("GET","/finduser");
+    request.send();
 }
 
-function updatePosts() {  // update post every 2 sec
+function updatePosts() {  //TODO adapt this function for new frontend
     const request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
@@ -27,4 +37,24 @@ function updatePosts() {  // update post every 2 sec
     request.send();
 }
 
+function post(){                    //christian is working on this
+    const titleTextBox = document.getElementById("titleBox");       //gets title of posts from the frontend
+    const title = titleTextBox.value;                               //actual text from the title box in frontend
+    const messageTextBox = document.getElementById("messageBox");   //gets message from post from the frontend
+    const message = messageTextBox.value;                           //actual text from the message box 
+    titleTextBox.value = "";                                        //resets the frontend to show a blank title box
+    messageTextBox.value = "";                                      //resets the frontend to show a blank message box
+    const request = new XMLHttpRequest();                           //generates an XMLHttpRequest for later use
+    request.onreadystatechange = function(){                        //when a 200 response is received, resumes functino here
+        console.log(this.response)                                  //placeholder for now
+    }
+    const postInfo = {"title":title,"message":message};             //creates a dictionary to hold the title and message from the frontend
+    request.open("POST","/add_post");                               //prepares request to send to python
+    request.send(JSON.stringify(postInfo));                         //sends jsonified dictionary with post content to python
+}
 
+function post_init(){//this function is loaded when the body of posts.html is loaded
+    user();
+    updatePosts();
+    setInterval(updatePosts,2000);//constantly gets posts
+}
